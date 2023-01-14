e62ab7dc8b3925b9e2aadb186947f9ae173fbecf
Sven Steinert <svensteinert96@gmail.com>
2023-01-14 16:48:46 +0100

    working com

--- MCU/src/main.cpp
+++ MCU/src/main.cpp
@@ -1,39 +1,55 @@
 #include <Arduino.h>
-#include <EEPROM.h>
+
+// RAM Storage
+#define RAM_STORAGE_SIZE 12000 // with 100 [Hz] results in 120 [s] max
+long *time_RAM; // 4 Bytes per entry
+double *value_RAM; // 4 Bytes per entry
+
+int index_RAM = 0;
+double val_buffer;
+double time_buffer;
+char val_buffer_str[50];
 
 byte incomingByte = 0; // for incoming serial data
-String databuffer = "time, value [mm]\n" ; // buffer for recording data
+String header = "time, value [mm]\n" ; // table head
+String databuffer = "" ; // buffer for recording data
+
 bool record = false;
+bool stream = false;
 unsigned long ref_time = 0;
 double value = 1.50040301;
-char val_buffer[50];
-char time_buffer[50];
-u_long addr = 0;
-int EEPROM_cache = 0;
-
-// define the number of bytes you want to access
-#define EEPROM_SIZE 400
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(500000);
-  EEPROM.begin(EEPROM_SIZE);
+  Serial.begin(921600);
+  // dynamically Allocate 48kB of RAM for time_RAM
+  time_RAM = new long[RAM_STORAGE_SIZE];
+  // dynamically Allocate 48kB of RAM for value_RAM
+  value_RAM = new double[RAM_STORAGE_SIZE];
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
   if (record){
-    dtostrf(value, 10, 8, val_buffer);
-    dtostrf(double(millis()-ref_time), 5, 0, time_buffer);
-    databuffer = time_buffer + String(",") + val_buffer + String("\n");
-    for (int i = 0; i < databuffer.length(); ++i)
-    {
-      EEPROM.write(addr, int(databuffer[i]));
-      ++addr;
-    }
-    EEPROM.commit();
+    if(index_RAM < RAM_STORAGE_SIZE){
+      // read ADC
+      // prepare value and time to write
+      time_buffer = (millis()-ref_time);
+      val_buffer = value;
 
+      time_RAM[index_RAM] = time_buffer;
+      value_RAM[index_RAM] = val_buffer;
+      ++index_RAM;
+
+      delay(10);
+      }
+    }
+  else if (stream){
+    // read ADC
+    val_buffer = value;
+    dtostrf(val_buffer, 10, 8, val_buffer_str);
+    Serial.println(val_buffer_str);
     delay(10);
   }
 
@@ -44,11 +60,11 @@ void loop() {
     switch(incomingByte) {
       case 48:
         // Start Recording = '0' = 48 [ASCII]
-        databuffer = "time, value [um]\n" ; // reset buffer
-        addr = 0;
+        index_RAM = 0; // reset buffer
         Serial.print("RECORDING");
         Serial.print("\nEND");
         record = true;
+        stream = false;
         ref_time = millis();
         break;
       case 49:
@@ -56,31 +72,36 @@ void loop() {
         Serial.print("STOPPED");
         Serial.print("\nEND");
         record = false;
+        stream = false;
         break;
       case 50:
         // Start Download = '2' = 50 [ASCII]
-        Serial.print("DOWNLOADING");
+        Serial.print("DOWNLOAD");
         Serial.print("\nEND");
-
-        //EEPROM_SIZE
-        for (int i = 0; i < EEPROM_SIZE; ++i)
-        {
-          EEPROM_cache = EEPROM.read(i);
-          Serial.print(char(EEPROM_cache));
-          //if (EEPROM_cache == 0x00)
-          //{
-          //  break;
-          //}
-        }
-        Serial.print("END");
+        
+        //Serial.print(header);
+        for (int i = 0; i < index_RAM; ++i)
+          {
+            Serial.print(time_RAM[i]);
+            Serial.print(',');
+            dtostrf(value_RAM[i], 10, 8, val_buffer_str);
+            Serial.print(val_buffer_str);
+            Serial.print('\n');
+          }
+        
+        Serial.print("END-OF-FILE-SEQUENCE\n");
         break;
+      case 51:
+        // Enable Stream Data = '3' = 51 [ASCII]
+        stream = true;
+        Serial.println("Stream Data Enabled");
+        break;
+      case 52:
+        // Disable Stream Data = '4' = 52 [ASCII]
+        stream = false;
+        Serial.println("Stream Data Disabled");
       default:
         break;
     }
-
-
-    // say what you got:
-    //Serial.print("I received: ");
-    //Serial.println(incomingByte);
   }
 }
--- app/cli.py
+++ app/cli.py
@@ -1,40 +1,92 @@
-import serial.tools.list_ports
-import serial
 import time
+import serial
+import serial.tools.list_ports
 
-ports = serial.tools.list_ports.comports()
+baudrate = 921600
 hardware_id = '10C4:EA60'
-for port, desc, hwid in sorted(ports):
-	if hardware_id in hwid:
-		ser = serial.Serial(port)  # open serial port
-		ser.close() 
-		ser = serial.Serial(port)  # open serial port
-		ser.baudrate = 500000
-		print(ser.name)         # check which port was really used
+connected = False
+con_dic = {True:'CONNECTED',False:'DISCONNECTED'}
+state = 'STOPPED'
+dev_port = ''
+infostr = ''
 
-		ser.write(b'0')     # write a string
-		ans = ser.read_until(b'\nEND')
-		print(ans)
+def p_reline(str=''):
+	global connected
+	global dev_port
+	global state
+	infostr = f' {con_dic[connected]} │ {port} │ {state:9} │ '
+	print(infostr + str,end='\r')
 
-		n = 5
-		for i in range(n):
-			print(f'{i}/{n}',end='\r',flush=True)
-			time.sleep(1)
+def start_connection(port):
+	global state
 
-		ser.write(b'1')     # write a string
-		ans = ser.read_until(b'\nEND')
-		print(ans)
+	ser = serial.Serial(port=port,baudrate=baudrate)  # open serial port
+	infostr = f' {con_dic[connected]} │ {port} │  {state}  │ '
+	n = input(infostr + 'Enter seconds to record: ')
+	try:
+		n = int(n)
+	except:
+		print(' ERROR: you didnt enter a valid number')
+		exit(0)
+	ser.write(b'0')     # write 0 = RECORD to MCU
+	ans = ser.read_until(b'\nEND')
+	try:
+		state = ans[:-4].decode('UTF-8')
+	except:
+		try:
+			state = ans[24:-4].decode('UTF-8')
+		except:
+			print(f'{ans} not decodable')
 
-		ser.write(b'2')     # write a string
-		ans = ser.read_until(b'\nEND')
-		print(ans)
-		data = ser.read_until(b'END').decode('UTF-8')
-		print(data)
-		with open('data.txt', 'w') as f:
-			f.write(data.split('END')[0])
-		print(ans)
-		ser.close()             # close port
-		break
+	for i in range(n):
+		p_reline(f'{i}/{n}')
+		time.sleep(1)
+	p_reline(f'{n}/{n}')
+
+	ser.write(b'1')     # write 1 = STOPP to MCU
+	ans = ser.read_until(b'\nEND')
+	state = ans[:-4].decode('UTF-8')
+	p_reline('       ')
+
+	ser.write(b'2')     # write 1 = DOWNLOAD to MCU
+	ans = ser.read_until(b'\nEND')
+	state = ans[:-4].decode('UTF-8')
+	p_reline()
+	cache = ''
+	while True:
+		data = ser.read_until(b'\n').decode('UTF-8')
+		if 'END-OF-FILE-SEQUENCE' in data:
+			break
+		p_reline(data.split(',')[0])
+		cache += data
+
+	with open('data.txt', 'w') as f:
+		f.write(cache)
+	print()
+	ser.close()             # close port
+	state = 'STOPPED'
+	p_reline()
+
+
+print("""
+┌───────────────────────────────────────────────────────────────────────┐
+│ ████ █████    High Precision LVDT Control                             │
+│  █ █ █ █ █                                                            │
+│  █ ███ █ █    https://github.com/Sven-J-Steinert/HighPrecisionLVDT	│
+└───────────────────────────────────────────────────────────────────────┘""")
+
+while True:
+	connected = False
+	while not connected:
+		ports = serial.tools.list_ports.comports()
+		for port, desc, hwid in sorted(ports):
+			print(f'DISCONNECTED',end='\r')
+			if hardware_id in hwid:
+				dev_port = port
+				connected = True
+		time.sleep(0.1)
+	start_connection(port)
+		
 
 
 
